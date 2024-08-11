55afd86f42939e35bcd52c5e9d6b4621e96ef986
Ajay Kumar <ajayetw2009@gmail.com>
2024-08-11 18:48:10 +0530

    setup file added.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Ajay Kumar',
+    author_email= 'ajayetw2009@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
